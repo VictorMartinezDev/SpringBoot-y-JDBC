906a6d41e98a85917bf88ea0553da1af7c8251f8
Victor Martinez <vicmanuelg19@gmail.com>
2025-12-17 17:31:38 -0600

    se agrega paquete service

--- src/main/java/com/mysql/springmysql/service/DataBaseService.java
+++ src/main/java/com/mysql/springmysql/service/DataBaseService.java
@@ -1,8 +1,7 @@
-package com.mysql.springmysql;
+package com.mysql.springmysql.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
